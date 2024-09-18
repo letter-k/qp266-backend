45846d5c9bc7d418802d23fd1a06852c8857f471
Kotorkovsciy <kotorkovsciy@yandex.ru>
2024-09-18 22:07:02 +0300

    fix: correct static dirs

--- config/settings/base.py
+++ config/settings/base.py
@@ -145,6 +145,13 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
 
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa: F405
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Media files
 
 MEDIA_ROOT = BASE_DIR / "media"
